13fd65a315450d7c30204b9edcc3567f888badc3
nethakha <tokuteikoway@gmail.com>
2020-05-17 16:45:03 +0900

    fix

--- vim.sh
+++ vim.sh
@@ -10,17 +10,19 @@ TIMEZONE="Tokyo"
 
 apt update > /dev/null
 
-dpkg -l | grep tzdata
+dpkg -l | grep tzdata > /dev/null
 if [ "$?" -eq 0 ] ; then
-	echo "TZDATA INSTALLED"
+	echo "tzdata is already installed."
 else
-	echo "INSTALL TZDATA"
+	echo "Install tzdata."
 	export DEBIAN_FRONTEND=noninteractive
 	ln -fs /usr/share/zoneinfo/$GEOAREA/$TIMEZONE /etc/localtime
 	apt install -y tzdata > /dev/null
 	dpkg-reconfigure --frontend noninteractive tzdata
+	echo "Installed tzdata."
 fi
 
+echo "Install the required tools and libraries."
 apt install -y sudo git make wget gcc \
   build-essential libbz2-dev libdb-dev \
   libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
@@ -46,64 +48,80 @@ make distclean
 
 which anyenv >/dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	echo "ANYENV INSTALLED"
+	echo "anyenv is already installed."
 else
+	echo "Install anyenv."
 	git clone https://github.com/anyenv/anyenv ~/.anyenv
 	echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bashrc
 	export PATH="$HOME/.anyenv/bin:$PATH"
 	echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
 	echo 'eval "$(anyenv init -)"' >> ~/.bashrc
 	source ~/.bash_profile
+	source ~/.bashrc
 	anyenv install --force-init
+	echo "Installed anyenv."
 fi
 
 which pyenv >/dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	echo "PYENV INSTALLED"
+	echo "pyenv is already installed."
 else
+	echo "Install pyenv."
 	anyenv install pyenv
 	source ~/.bash_profile
+	source ~/.bashrc
+	echo "Installed pyenv."
 fi
 
 which rbenv >/dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	echo "RBENV INSTALLED"
+	echo "rbenv is already installed."
 else
+	echo "Install rbenv."
 	anyenv install rbenv
 	source ~/.bash_profile
+	source ~/.bashrc
+	echo "Installed rbenv."
 fi
 
 
 which python >/dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	echo "PYTHON2 INSTALLED"
+	echo "Python2 is already installed."
 else
+	echo "Install Python2."
 	CONFIGURE_OPTS="--enable-shared" pyenv install $PY2
+	echo "Installed Python2."
 fi
 
 which python3 >/dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	echo "PYTHON3 INSTALLED"
+	echo "Python3 is already installed."
 else
+	echo "Install Python3."
 	CONFIGURE_OPTS="--enable-shared" pyenv install $PY3
+	echo "Installed Python3."
 fi
 
 which ruby >/dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	echo "RUBY INSTALLED"
+	echo "Ruby is already installed."
 else
+	echo "Install Ruby."
 	rbenv install $RUBY
+	echo "Installed Ruby."
 fi
 
+echo "Set global."
 pyenv global $PY2 $PY3
 rbenv global $RUBY
 
+
+set -e
 py2conf=`python -c "import distutils.sysconfig; print distutils.sysconfig.get_config_var('LIBPL')"`
 py3conf=`python3 -c "import distutils.sysconfig; print(distutils.sysconfig.get_config_var('LIBPL'))"`
 
-set -e
-
-echo "CONFIG"
+echo "Configure Vim's configuration."
 LDFLAGS="-Wl,-rpath=${HOME}/.anyenv/envs/pyenv/versions/$PY2/lib:${HOME}/.anyenv/envs/pyenv/versions/$PY3/lib:${HOME}/.anyenv/envs/rbenv/versions/$RUBY/lib" ./configure \
     --with-features=huge \
     --enable-fail-if-missing \
@@ -124,8 +142,11 @@ LDFLAGS="-Wl,-rpath=${HOME}/.anyenv/envs/pyenv/versions/$PY2/lib:${HOME}/.anyenv
     --disable-gui \
     --disable-xsmp
 
+echo "Run make."
 make
+echo "Run make install."
 make install
+echo "Displays the version of Vim."
 vim --version
 echo 'vim -c "python print(sys.version)"'
 echo 'vim -c "python3 print(sys.version)"'
